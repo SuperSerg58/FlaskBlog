41bf19ca9042b09bdac08c44c2404ce25bdd6e45
Alexeev_S <demiurg58@gmail.com>
2020-01-04 17:57:15 +0300

    add database in Flask-App

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
 from webapp.weather import weather_by_city
-from webapp.python_org_news import get_python_news
+
+from webapp.model import db, News
 
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_pyfile('config.py')
+    app = Flask(__name__)  # Создаём приложение фласк
+    app.config.from_pyfile('config.py')  # Подключается файл конфигурации
+    db.init_app(app)  # Инициализируется База Данных, которая подсасывает константу из конфига
 
     @app.route('/')
     def index():
-        title = 'Blog'
+        title = 'Main Page'
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
-        news_list = get_python_news()
+        news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', title=title, weather=weather, news_list=news_list)
 
     return app
--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
+from webapp.model import db, News
+
 
 def get_html(url):
     try:
@@ -18,20 +20,22 @@ def get_python_news():
     if html:
         soup = BeautifulSoup(html, 'html.parser')
         all_news = soup.find('ul', class_='list-recent-posts').findAll('li')
-        result_news = []
         for news in all_news:
             title = news.find('a').text
             url = news.find('a')['href']
             published = news.find('time').text
             try:
-                published = datetime.strptime(published, '%b. %d, %Y').strftime('%Y-%m-%d')
+                published = datetime.strptime(published, '%b. %d, %Y')
             except(ValueError):
                 published = datetime.now()
 
-            result_news.append({
-                "title": title,
-                "url": url,
-                "published": published
-            })
-        return result_news
-    return False
+            save_news(title, url, published)
+
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        news_news = News(title=title, url=url, published=published)
+        db.session.add(news_news)
+        db.session.commit()
