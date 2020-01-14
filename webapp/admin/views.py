71373259ca7142ed66acf65229f3af604e0a71f6
Sergey Alexeev <demiurg58@gmail.com>
2020-01-14 18:13:17 +0300

    add decorators for check admin

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -1,13 +1,10 @@
 from flask import Blueprint
-from flask_login import current_user, login_required
+from webapp.user.decorators import admin_required
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')  # имя_сайта/users/login
 
 
 @blueprint.route('')
-@login_required
+@admin_required
 def admin_index():
-    if current_user.is_admin:
-        return 'Hello Admin'
-    else:
-        return 'You are not Admin'
+    return 'Hello Admin'
