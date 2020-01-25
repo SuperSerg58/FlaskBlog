f6e55f4aea7184493e19139daafb0190a4840579
Alexeev_S <demiurg58@gmail.com>
2020-01-25 12:23:28 +0300

    add tasks

--- get_all_news.py
+++
@@ -1,7 +0,0 @@
-from webapp import create_app
-from webapp.news.parsers.habr import get_news_snippents, get_news_conntent
-
-app = create_app()
-with app.app_context():
-    #get_news_snippents()
-    get_news_conntent()
--- tasks.py
+++ tasks.py
@@ -1,10 +1,10 @@
 from celery import Celery
-
+from celery.schedules import crontab
 from webapp import create_app
 from webapp.news.parsers.habr import get_news_snippents, get_news_conntent
 
 flask_app = create_app()
-celery_app = Celery('tasks', broker='redis://localhost:6379/0')
+celery_app = Celery('tasks', broker='redis://127.0.0.1:6379/0')
 
 
 @celery_app.task
@@ -17,3 +17,9 @@ def habr_snippets():
 def habr_content():
     with flask_app.app_context():
         get_news_conntent()
+
+
+@celery_app.on_after_configure.connect
+def setup_pereodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(crontab(minute='*/1'), habr_snippets.s())
+    sender.add_periodic_task(crontab(minute='*/1'), habr_content.s())
