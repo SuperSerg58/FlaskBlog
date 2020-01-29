5f8608e7d395cea46b1dcd293fcca140f6a981db
Sergey Alexeev <demiurg58@gmail.com>
2020-01-29 17:09:45 +0300

    add comment model migrate

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -1,4 +1,6 @@
 from webapp.db import db
+from datetime import datetime
+from sqlalchemy.orm import relationship
 
 
 class News(db.Model):
@@ -10,3 +12,25 @@ class News(db.Model):
 
     def __repr__(self):
         return '<News {} {}>'.format(self.title, self.url)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    news_id = db.Column(
+        db.Integer,
+        db.ForeignKey('news.id', ondelete='CASCADE'),
+        index=True
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )
+
+    news = relationship('News', backref='comments')
+    user = relationship('User', backref='comments')
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.id)
